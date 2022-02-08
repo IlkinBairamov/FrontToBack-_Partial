cad4f0e1ca5d10b8ba104642ba26cc5159025ff4
IlkinBairamov <ilkinabr@code.edu.az>
2022-02-08 05:47:42 -0800

    Homework

---
+++ FrontToBack _Partial/FrontToBack/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using FrontToBack.DataAccessLayer;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public HomeController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var sliderImage = _dbContext.SliderImages.ToList();
+            var slider = _dbContext.Sliders.SingleOrDefault();
+            var category = _dbContext.Categories.ToList();
+            var product = _dbContext.Products.Include(x=>x.Category).Take(4).ToList();
+            var aboutsection = _dbContext.AboutSections.SingleOrDefault();
+            var aboutsectionimage = _dbContext.AboutSectionImages.SingleOrDefault();
+            var aboutsectionlist = _dbContext.AboutSectionLists.ToList();
+            var expertsection = _dbContext.ExpertSections.SingleOrDefault();
+            var expertsectionlist = _dbContext.ExpertSectionLists.ToList();
+            var blog = _dbContext.Blogs.SingleOrDefault();
+            var bloglist = _dbContext.BlogLists.ToList();
+            var slidersay = _dbContext.SliderSays.ToList();
+            var subscribe = _dbContext.Subscribes.SingleOrDefault();
+            var instagram = _dbContext.Instagrams.ToList();
+
+            return View(new HomeViewModel { 
+                SliderImage=sliderImage,
+                Slider=slider,
+                Categories=category,
+                Products=product,
+                AboutSections=aboutsection,
+                AboutSectionImages=aboutsectionimage,
+                AboutSectionLists=aboutsectionlist,
+                ExpertSections =expertsection,
+                ExpertSectionLists=expertsectionlist,
+                Blogs=blog,
+                BlogLists=bloglist,
+                SliderSays=slidersay,
+                Subscribes=subscribe,
+                Instagrams=instagram
+            });
+        }
+    }
+}
---
+++ FrontToBack _Partial/FrontToBack/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using FrontToBack.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.Controllers
+{
+    public class ProductsController : Controller    
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly int _productsCount;
+        public ProductsController(AppDbContext dbContext)
+        {   
+            _dbContext = dbContext;
+            _productsCount = _dbContext.Products.Count();
+        }
+        public IActionResult Index()
+        {
+            ViewBag.ProductsCount = _productsCount;
+            var products = _dbContext.Products.Include(x => x.Category).Take(4).ToList();
+            return View(products);
+        }
+
+        public IActionResult Scroll(int skip)   
+        {
+            if (skip >= _productsCount)
+            {
+                return BadRequest();
+            }
+            var product = _dbContext.Products.Include(x => x.Category).Skip(skip).Take(4).ToList();
+
+            return PartialView("_ProductPartial", product);
+        }
+    }
+}
